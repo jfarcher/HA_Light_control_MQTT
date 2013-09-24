4e9ff3fa236d906b1d386e9c44881d31b05a90a0
jfarcher <jon@rosslug.org.uk>
2013-09-24 01:18:40 +0100

    Code wasn't quite complete and mac address creation wasn't working 100%, resolved this by copying all code accross correctly.

--- mqttlightcontrol_lightside.ino
+++ mqttlightcontrol_lightside.ino
@@ -11,7 +11,7 @@
 
 #define MQTT_SERVER "192.168.1.3"
 
-byte mac[]= { 0x91, 0xA3, 0xAA, 0x00, 0x01, 0x03 };
+byte mac[]= { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
 OneWire ds(2);
 IPAddress ip(192,168,1, 177);
 IPAddress gateway(192,168,1, 1);
@@ -32,33 +32,41 @@ void setup() {
   
   Serial.begin(9600);
   delay( 500 );
-  Serial.println ("Searching for DS18B20...");
+  Serial.print ("Searching for DS18B20...");
   ds.reset_search(); // Start the search with the first device
   
   if ( !ds.search(dsAddress) )
   {
-      Serial.println("none found. Using specified MAC Address.");
+    Serial.println("none found. Using specified MAC Address.");
   } else {
-    Serial.println( "success. Setting MAC address:" );
-    Serial.print("DS18B20 ROM =");
-    for( i = 0; i < 8; i++)
-    {
-      Serial.write(' ');
-      Serial.print( dsAddress[i], HEX);
-    }
-  Serial.println();  
+    Serial.print( "Success! \nSetting MAC address...." );
+
   mac[1] = dsAddress[3];
   mac[2] = dsAddress[4];
   mac[3] = dsAddress[5];
   mac[4] = dsAddress[6];
   mac[5] = dsAddress[7];
   }
-  Serial.print ("Ethernet MAC=");
-  Serial.print ("Eth: ");
-  Ethernet.begin(mac,ip,gateway,subnet); 
-  Serial.print("Network connected: ");     
-  Serial.println(Ethernet.localIP());
-
+  Serial.print( "Ethernet MAC =" );
+  for( i = 0; i < 6; i++ )
+  {
+    Serial.write( ' ' );
+    Serial.print( mac[i], HEX );
+  }
+  Serial.println();
+  
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    for(;;)
+      ;
+  }
+Serial.print("IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print("."); 
+  }
+  Serial.println();
   if (client.connect("arduinoClient")) {      // connecting to MQTT server
     client.subscribe("device/1/led");             // subscribe to topic for led 1
     client.subscribe("device/0/led");             // subscribe to topic for led 0
